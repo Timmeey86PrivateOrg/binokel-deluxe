ad77a576c30e0eea988dca1d4fbd571cefb6e79f
Timmeey86 <Timmeey86@users.noreply.github.com>
2019-04-13 22:34:17 +0200

    Added prototype code for settings and started implementation

---
+++ src/BinokelDeluxe.Shared/IConfigurable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinokelDeluxe.Shared
+{
+    /// <summary>
+    /// This interface is used to identify classes which store settings.
+    /// Since reflections will be used to work with these classes, the interface currently does not define any methods.
+    /// </summary>
+    public interface IConfigurable
+    {
+    }
+}
