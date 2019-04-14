c1c2bdb61072a271475b41d99b19216d4a61d6e8
Timmeey86 <Timmeey86@users.noreply.github.com>
2019-04-14 10:21:52 +0200

    Prepared state machine dependencies

---
+++ src/BinokelDeluxe.GameLogic/GameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinokelDeluxe.GameLogic
+{
+    /// <summary>
+    /// This class makes use of a state machine to store the current game state.
+    /// </summary>
+    public class GameState
+    {
+    }
+}
