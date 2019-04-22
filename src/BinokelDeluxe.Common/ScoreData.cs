d4c65ee9fd5eda40b2500714b2b799d3f6e13704
Timmeey86 <Timmeey86@users.noreply.github.com>
2019-04-22 22:27:26 +0200

    Finished first UI interface version

---
+++ src/BinokelDeluxe.Common/ScoreData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinokelDeluxe.Common
+{
+    /// <summary>
+    /// The type of the score.
+    /// </summary>
+    public enum ScoreType
+    {
+        PlayerScore,
+        TeamScore
+    }
+
+    /// <summary>
+    /// Contains the score of a single player or team (in case of cross-Binokel).
+    /// This is a struct, so you need to pass it as a ref object if you want to modify it.
+    /// </summary>
+    public struct ScoreData
+    {
+        /// <summary>
+        /// The type of the score.
+        /// </summary>
+        public ScoreType ScoreType { get; set; }
+        /// <summary>
+        /// The points of the bidding phase in case the player or team won that phase.
+        /// </summary>
+        public short BiddingPoints { get; set; }
+        /// <summary>
+        /// The amount of points which were melded.
+        /// </summary>
+        public short MeldedPoints { get; set; }
+        /// <summary>
+        /// The amount of points received for winning tricks.
+        /// </summary>
+        public short TrickPoints { get; set; }
+        /// <summary>
+        /// Extra points which were received, e.g. because a player went out, or a Bettel or Durch was won or lost.
+        /// This value may be negative. 
+        /// </summary>
+        public short ExtraPoints { get; set; }
+        /// <summary>
+        /// The amount of points for the current round. Note that melded points are only counted if at least one trick was won.
+        /// This value may be negative. 
+        /// </summary>
+        public short RoundScore { get; set; }
+        /// <summary>
+        /// The total score of all rounds played so far.
+        /// This value may be negative. 
+        /// </summary>
+        public short TotalScore { get; set; }
+    }
+}
--- src/BinokelDeluxe.UI/IUserInterface.cs
+++ src/BinokelDeluxe.UI/IUserInterface.cs
@@ -8,6 +8,7 @@ namespace BinokelDeluxe.UI
     /// Defines the interface for accessig the User Interface.
     /// Note: If UI updates are performed in a different thread (recommended), interface implementations should block the calling thread until the UI finished its work.
     /// This is vital since code in the calling thread will assume the UI is idle whenever a method returns.
+    /// Note: This interface is rather large. Feel free to implement an adapter which delegates work to several smaller worker classes.
     /// </summary>
     public interface IUserInterface
     {
@@ -21,20 +22,19 @@ namespace BinokelDeluxe.UI
         /// Implementers should play a dealing animation, with all cards covered.
         /// </summary>
         /// <param name="dealerPosition">The position of the dealer, where 0 is the position of the device user.</param>
-        /// <param name="cardsByPlayers">A two-dimensional collection of cards where the outer dimension is the position of the player on the table (where
-        /// position 0 = user) and the inner dimension is the collection of cards (10 or 12, dependent on settings) the player received.
-        /// The UI should list these cards in the order provided by the IEnumerable.</param>
+        /// <param name="numberOfCardsPerPlayer">The amount of cards for each player.</param>
         /// <param name="numberOfCardsInDabb">The amount of cards which shall be displayed in the dabb.</param>
         void PlayDealingAnimation(
             int dealerPosition,
-            IEnumerable<IEnumerable<Common.Card>> cardsByPlayers,
+            int numberOfCardsPerPlayer,
             int numberOfCardsInDabb
             );
         /// <summary>
         /// Implementers should uncover the cards of the user for himself.
-        /// The relevant cards were provided in PlayDealingAnimation which is guaranteed to be called before this method.
+        /// The cards should be displayed in the order provided by userCards, if possible.
         /// </summary>
-        void UncoverCardsForUser();
+        /// <param name="userCards">The cards of the user.</param>
+        void UncoverCardsForUser(IEnumerable<Common.Card> userCards);
         /// <summary>
         /// Implementers should ask the user to either make the first bid or pass. This may or may not be called for the player, dependent on their position and the actions
         /// performed by anyone being allowed to decide before them.
@@ -76,5 +76,48 @@ namespace BinokelDeluxe.UI
         /// <param name="trumpSuit">Implementers need to store the suit which was selected as trump, or null in case of a Bettel or Durch.</param>
         /// <returns>TrumpSelected, GoingOut, BettelAnnounced or DurchAnnounced.</returns>
         Common.GameTrigger LetUserExchangeCardsWithDabb(out IEnumerable<Common.Card> discardedCards, out Common.CardSuit? trumpSuit);
+        /// <summary>
+        /// Implementers should rearrange (or remove and re-initialize) the cards of the player in accordance with the order in rearrangedCards.
+        /// </summary>
+        /// <param name="rearrangedCards">The new order of cards of the human player.</param>
+        void RearrangeCardsForUser(IEnumerable<Common.Card> rearrangedCards);
+        /// <summary>
+        /// Implementes should display the given melds. The enumerable contains one entry for each player, ordered by player position on the table
+        /// (i.e. independent of who is the current dealer). The UI is free to decide whether a list of all cards, or the single melds as groups are displayed.
+        /// </summary>
+        /// <param name="meldsByPlayers">The melds made by each player.</param>
+        void DisplayMelds(IEnumerable<Common.MeldData> meldsByPlayers);
+        /// <summary>
+        /// Implementers should display a scoreboard for the current round as a result of the player who won the bidding phase going out.
+        /// The list of score data will either contain data for three players or two teams.
+        /// </summary>
+        /// <param name="playerOrTeamScores">The scores of the players or teams, where the first player (or team) is the user.</param>
+        void DisplayGoingOutScore(IEnumerable<Common.ScoreData> playerOrTeamScores);
+        /// <summary>
+        /// Implementers should let the user select a card to be played. The card should not be removed from the player's hand yet.
+        /// </summary>
+        /// <returns>The card which was selected by the user.</returns>
+        Common.Card LetUserSelectCard();
+        /// <summary>
+        /// Implementers should let the user know they attempted to play an invalid card and should highlight the valid cards in a way.
+        /// </summary>
+        /// <param name="validCards">The cards which are allowed to be played.</param>
+        void HandleInvalidMove(IEnumerable<Common.Card> validCards);
+        /// <summary>
+        /// Implementers should remove any currently displayed validity states from the UI.
+        /// </summary>
+        void RemoveValidityState();
+        /// <summary>
+        /// Implementers should show an animation that moves the given card from the given player to the middle, while uncovering it for all players.
+        /// This is also sent for human players.
+        /// </summary>
+        /// <param name="playerPosition">The position of of the player on the table where 0 is the user.</param>
+        /// <param name="card">The card which shall be placed.</param>
+        void PlaceCardInMiddle(int playerPosition, Common.Card card);
+        /// <summary>
+        /// Implementers should display a scoreboard for the current round as a result of all cards being placed.
+        /// </summary>
+        /// <param name="playerOrTeamScores">The scores of the players or teams, where the first player (or team) is the user.</param>
+        void DisplayRoundScore(IEnumerable<Common.ScoreData> playerOrTeamScores);
     }
 }
