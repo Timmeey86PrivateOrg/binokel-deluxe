85764d2922ed202c1118f7968d3eb572c4ab2ccc
Timmeey86 <Timmeey86@users.noreply.github.com>
2019-05-05 13:05:00 +0200

    Fixed or ignored style cop warnings

--- src/BinokelDeluxe.Core.Test/GameStateStackTest.cs
+++ src/BinokelDeluxe.Core.Test/GameStateStackTest.cs
@@ -1,11 +1,17 @@
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Xml.Serialization;
-
 namespace BinokelDeluxe.Core.Test
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Xml.Serialization;
+    using NUnit.Framework;
+
+#pragma warning disable SA1600 // Test methods should have an expressive name rather than being documented.
+#pragma warning disable CS1591 // Same reason as above
+
+    /// <summary>
+    /// This class tests the Game State Stack.
+    /// </summary>
     public class GameStateStackTest
     {
         [Test]
@@ -13,15 +19,15 @@ namespace BinokelDeluxe.Core.Test
         {
             var expectedGameCreationInfo = new Core.GameCreationInfo()
             {
-                AIStrategyTypes = new List<String>()
+                AIStrategyTypes = new List<string>()
                 {
                     typeof(AI.TempAI1).FullName,
                     typeof(AI.TempAI2).FullName,
-                    typeof(AI.TempAI3).FullName
+                    typeof(AI.TempAI3).FullName,
                 },
                 RandomSeed = 82745392,
                 RuleSettings = new GameLogic.RuleSettings(),
-                Version = new Common.SerializableVersion(1, 0)
+                Version = new Common.SerializableVersion(1, 0),
             };
             var expectedGameStateStack = new Core.GameStateStack()
             {
@@ -30,17 +36,17 @@ namespace BinokelDeluxe.Core.Test
                 {
                     new GameStateChangeInfo()
                     {
-                        HumanTrigger = Common.GameTrigger.BidPlaced
+                        HumanTrigger = Common.GameTrigger.BidPlaced,
                     },
                     new GameStateChangeInfo()
                     {
                         CardPlayedByHuman = new Common.Card()
                         {
                             Suit = Common.CardSuit.Acorns,
-                            Type = Common.CardType.King
-                        }
-                    }
-                }
+                            Type = Common.CardType.King,
+                        },
+                    },
+                },
             };
 
             var xmlSerializer = new XmlSerializer(typeof(Core.GameStateStack));
@@ -60,14 +66,13 @@ namespace BinokelDeluxe.Core.Test
             {
                 DeckNumber = 0,
                 Suit = Common.CardSuit.Acorns,
-                Type = Common.CardType.Ace
+                Type = Common.CardType.Ace,
             };
 
             Assert.That(!card.Equals(null));
             Assert.AreNotEqual(
                 Common.ValueComparer<Common.Card>.GetHashCode(card),
-                Common.ValueComparer<Common.Card>.GetHashCode(null)
-                );
+                Common.ValueComparer<Common.Card>.GetHashCode(null));
         }
 
         [Test]
@@ -77,14 +82,14 @@ namespace BinokelDeluxe.Core.Test
             {
                 DeckNumber = 0,
                 Suit = Common.CardSuit.Bells,
-                Type = Common.CardType.Unter
+                Type = Common.CardType.Unter,
             };
 
             var card2 = new Common.Card()
             {
                 DeckNumber = 0,
                 Suit = Common.CardSuit.Bells,
-                Type = Common.CardType.Unter
+                Type = Common.CardType.Unter,
             };
 
             Assert.That(card1.Equals(card2));
@@ -99,14 +104,14 @@ namespace BinokelDeluxe.Core.Test
             {
                 DeckNumber = 0,
                 Suit = Common.CardSuit.Hearts,
-                Type = Common.CardType.Ober
+                Type = Common.CardType.Ober,
             };
 
             var card2 = new Common.Card()
             {
                 DeckNumber = 1,
                 Suit = Common.CardSuit.Hearts,
-                Type = Common.CardType.Ober
+                Type = Common.CardType.Ober,
             };
 
             Assert.That(!card1.Equals(card2));
@@ -121,14 +126,14 @@ namespace BinokelDeluxe.Core.Test
             {
                 DeckNumber = 0,
                 Suit = Common.CardSuit.Hearts,
-                Type = Common.CardType.Ober
+                Type = Common.CardType.Ober,
             };
 
             var card2 = new Common.Card()
             {
                 DeckNumber = 0,
                 Suit = Common.CardSuit.Leaves,
-                Type = Common.CardType.Ober
+                Type = Common.CardType.Ober,
             };
 
             Assert.That(!card1.Equals(card2));
@@ -143,14 +148,14 @@ namespace BinokelDeluxe.Core.Test
             {
                 DeckNumber = 0,
                 Suit = Common.CardSuit.Hearts,
-                Type = Common.CardType.Ober
+                Type = Common.CardType.Ober,
             };
 
             var card2 = new Common.Card()
             {
                 DeckNumber = 0,
                 Suit = Common.CardSuit.Hearts,
-                Type = Common.CardType.Unter
+                Type = Common.CardType.Unter,
             };
 
             Assert.That(!card1.Equals(card2));
@@ -159,3 +164,5 @@ namespace BinokelDeluxe.Core.Test
         }
     }
 }
+#pragma warning disable CS1591 // Same reason as above
+#pragma warning restore SA1600 // Elements should be documented
