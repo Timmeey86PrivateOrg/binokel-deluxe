98c3f68a8210e1e9785c620581cc500560ae4479
Timmeey86 <Timmeey86@users.noreply.github.com>
2019-05-05 13:55:17 +0200

    Fixed remaining style cop warnings except for game controller

--- src/BinokelDeluxe.Learning/LearningSettings.cs
+++ src/BinokelDeluxe.Learning/LearningSettings.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BinokelDeluxe.Learning
+﻿namespace BinokelDeluxe.Learning
 {
+    /// <summary>
+    /// This class will contain settings for the learning mode in the future.
+    /// </summary>
     public class LearningSettings : Common.IConfigurable
     {
     }
